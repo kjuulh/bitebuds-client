2b18621a23b48883c85ec0f772a536b5d82c902a
kjuulh <contact@kjuulh.io>
2023-03-05 00:15:58 +0100

    fix: remove unused pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,6 @@
 </script>
 
 <main class="space-y-4 pt-8">
-	<div class="flex space-x-2 hidden">
-		<a class="underline" href="prev">Prev</a>
-		<p>9</p>
-		<a class="underline" href="next">Next</a>
-	</div>
-
 	<ul class="days flex flex-col">
 		<li><Day day="monday" next={true} /></li>
 		<li><Day day="tuesday" /></li>
